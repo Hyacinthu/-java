84bb3513e6f9ad27ca2a2484679ec055ac0b278f
Hyacinthu <34212274+Hyacinthu@users.noreply.github.com>
2019-07-30 15:08:50 +0800

    Create HelloTest.java

---
+++ src/HelloTest.java
@@ -0,0 +1,18 @@
+/**
+ * <p>Title:HelloTest.java </p>
+ * <p>Description: </p>
+ * @authorASUS
+ * @dateÏÂÎç3:07:53
+ */
+
+/**
+ * <p>Title:HelloTest </p>
+ * <p>Description: </p>
+ * @authorASUS
+ * @dateÏÂÎç3:07:53
+ */
+public class HelloTest {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
